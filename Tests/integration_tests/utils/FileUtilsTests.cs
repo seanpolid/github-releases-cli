b0603eefd407ed47baba076b3d15ad017adafee2
seanpolid <seanpolidori0@gmail.com>
2024-01-19 22:13:00 -0500

    Implemented zip functionality and tested it

---
+++ Tests/integration_tests/utils/FileUtilsTests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitHubReleasesCLI.utils;
+
+namespace Tests.integration_tests.utils
+{
+    public class FileUtilsTests : IDisposable
+    {
+        private readonly string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        private string? expectedDirectory;
+        private string? testDirectory;
+        private string? file1Path;
+        private string? file2Path;
+        private string? file3Path;
+
+        public FileUtilsTests() 
+        {
+            CreateTestFilesAndFolders();
+        }
+
+        private void CreateTestFilesAndFolders()
+        {
+            expectedDirectory = Path.Combine(baseDirectory, "expected");
+            Directory.CreateDirectory(expectedDirectory);
+
+            testDirectory = Path.Combine(baseDirectory, "test");
+            Directory.CreateDirectory(testDirectory);
+
+            file1Path = Path.Combine(baseDirectory, "file1.txt");
+            file2Path = Path.Combine(testDirectory, "file2.txt");
+            file3Path = Path.Combine(testDirectory, "file3.txt");
+
+            File.Create(file1Path).Close();
+            File.Create(file2Path).Close();
+            File.Create(file3Path).Close();
+        }
+
+        public void Dispose()
+        {
+            Directory.Delete(expectedDirectory, true);
+            Directory.Delete(testDirectory, true);
+            File.Delete(file1Path);
+
+            IEnumerable<string> zipFilesToDelete = Directory.EnumerateFiles(baseDirectory)
+                                                            .Where(file => file.Contains(".zip"))
+                                                            .ToList();
+            foreach(string zipFile in zipFilesToDelete)
+            {
+                File.Delete(zipFile);
+            }
+        }
+
+        [Fact]
+        public void Zip_RelativeFile_Success()
+        {
+            // Arrange
+            string path = "./file1.txt";
+
+            string zipFilePath = Path.Combine(baseDirectory, "asset.zip");
+
+            // Act
+            FileUtils.Zip(path);
+
+            // Assert
+            Assert.True(File.Exists(zipFilePath));
+
+            ZipFile.ExtractToDirectory(zipFilePath, expectedDirectory);
+            Assert.True(
+                File.Exists(Path.Combine(expectedDirectory, "file1.txt"))
+            );
+        }
+
+        [Fact]
+        public void Zip_AbsoluteFile_Success()
+        {
+            // Arrange
+            string path = Path.Combine(baseDirectory, "file1.txt");
+
+            string zipFilePath = Path.Combine(baseDirectory, "asset.zip");
+
+            // Act
+            FileUtils.Zip(path);
+
+            // Assert
+            Assert.True(File.Exists(zipFilePath));
+
+            ZipFile.ExtractToDirectory(zipFilePath, expectedDirectory);
+            Assert.True(
+                File.Exists(Path.Combine(expectedDirectory, "file1.txt"))
+            );
+        }
+
+        [Fact]
+        public void Zip_Folder_Success()
+        {
+            // Assert
+            string zipFileName = Path.GetFileName(testDirectory) + ".zip";
+            string zipFilePath = Path.Combine(baseDirectory, zipFileName);
+            
+            // Act
+            FileUtils.Zip(testDirectory);
+
+            //Assert
+            Assert.True(File.Exists(zipFilePath));
+
+            ZipFile.ExtractToDirectory(zipFilePath, expectedDirectory);
+            AssertDirectoriesAreEqual(expectedDirectory, testDirectory);
+        }
+
+        private static void AssertDirectoriesAreEqual(string parentFolder, string testDirectory)
+        {
+            // Make sure base directory is present
+            string[] directories = Directory.GetDirectories(parentFolder);
+            Assert.Single(directories);
+            Assert.Equal(Path.GetFileName(directories[0]), Path.GetFileName(testDirectory));
+
+            // Make sure the nested files are the same
+            HashSet<string> actualFiles = Directory.GetFiles(directories[0])
+                                                   .Select(path => Path.GetFileName(path))
+                                                   .ToHashSet();
+            HashSet<string> expectedFiles = Directory.GetFiles(testDirectory)
+                                                     .Select(path => Path.GetFileName(path))
+                                                     .ToHashSet();
+            Assert.True(expectedFiles.SetEquals(actualFiles));
+        }
+
+        [Fact]
+        public void Zip_NonExistantPath_Exception()
+        {
+            // Arrange
+            string path = "nonexistant-file.txt";
+
+            // Act and Assert
+            Assert.Throws<FileNotFoundException>(() =>
+            {
+                FileUtils.Zip(path);
+            });
+        }
+    }
+}
--- utils/FileUtils.cs
+++ utils/FileUtils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,46 @@ namespace GitHubReleasesCLI.utils
 {
     public class FileUtils
     {
+        /// <summary>
+        /// Zips the contents found at the specified path. If it is a single file, it will
+        /// create a new file called 'asset.zip'. Otherwise, it will use the name of the folder 
+        /// for the zip file's name and it will include the base directory in the zip.
+        /// </summary>
+        /// <param name="path"></param>
+        public static void Zip(string path)
+        {
+            string baseDirectory = Directory.GetCurrentDirectory();
+            if (!Path.IsPathRooted(path))
+            {
+                path = Path.Combine(baseDirectory, path);
+            }
+
+            if (File.Exists(path))
+            {
+                string zipPath = Path.Combine(baseDirectory, "asset.zip");
+                if (File.Exists(zipPath))
+                {
+                    File.Delete(zipPath);
+                }
+
+                using ZipArchive zip = ZipFile.Open(zipPath, ZipArchiveMode.Create);
+                zip.CreateEntryFromFile(path, Path.GetFileName(path));
+            }
+            else if (Directory.Exists(path))
+            {
+                string zipFile = $"{path}.zip";
+                string zipPath = Path.Combine(baseDirectory, zipFile);
+                if (File.Exists(zipPath))
+                {
+                    File.Delete(zipPath);
+                }
+
+                ZipFile.CreateFromDirectory(path, zipPath, CompressionLevel.Optimal, true);
+            }
+            else
+            {
+                throw new FileNotFoundException($"{path} does not exist");
+            }
+        }
     }
 }
