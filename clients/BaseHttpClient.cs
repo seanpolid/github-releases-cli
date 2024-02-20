8d89cf830dbf309f4b3219e372218edd379fba30
seanpolid <seanpolidori0@gmail.com>
2024-02-19 23:41:16 -0500

    Revert "Removed timeout"
    This reverts commit 5f23baadeba81889c56a995b0baadbd3aa0c2ef5.

--- clients/BaseHttpClient.cs
+++ clients/BaseHttpClient.cs
@@ -2,7 +2,13 @@
 {
     public class BaseHttpClient
     {
-        private readonly HttpClient httpClient = new();
+        private readonly HttpClient httpClient;
+
+        public BaseHttpClient()
+        {
+            httpClient = new();
+            httpClient.Timeout = TimeSpan.FromMinutes(5);
+        }
 
         public virtual HttpResponseMessage Send(HttpRequestMessage httpRequestMessage)
         {
